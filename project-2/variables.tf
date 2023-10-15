fd3dcdfa5b6cdb0bdfb39e869506009dc05f24ea
MomenHamza91 <147370493+MomenHamza91@users.noreply.github.com>
2023-10-15 12:30:37 +0300

    Add files via upload

---
+++ project-2/variables.tf
@@ -0,0 +1,16 @@
+variable "vra_url" {
+ type = "string"
+ description = "Enter The url of VRA"
+ 
+}
+variable "vra_refresh_token" {
+ type = "string"
+ description = "Enter The url of VRA"
+
+}
+variable "blueprint_name" {
+ type = "string"
+ description = "Blue Print Name"
+ default = "TerraformBPTest"
+}
+
