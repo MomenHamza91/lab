b642aa007428921879500a925c9ace909dece5c2
MomenHamza91 <147370493+MomenHamza91@users.noreply.github.com>
2023-10-15 16:20:04 +0300

    Add files via upload

---
+++ nspovra/main.tf
@@ -0,0 +1,26 @@
+
+resource "vra_blueprint" "terraformbptest" {
+  name        = var.blueprint_name
+  description = "Created by vRA terraform provider"
+
+  project_id = vra_project.terraformbptest.id
+
+  content = <<-EOT
+    formatVersion: 1
+    inputs:
+      image:
+        type: string
+        description: "Image"
+      flavor:
+        type: string
+        description: "Flavor"
+    resources:
+      Machine:
+        type: Cloud.Machine
+        properties:
+          image: $${input.image}
+          flavor: $${input.flavor}
+		  constraints:
+           - tag: env:prod
+  EOT
+}
---
+++ nspovra/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    vra = {
+      source = "vmware/vra"
+    }
+  }
+  required_version = ">= 0.13"
+}
+provider "vra" {
+  url           = var.vra_url
+  refresh_token = var.vra_refresh_token
+  insecure      = false
+}
